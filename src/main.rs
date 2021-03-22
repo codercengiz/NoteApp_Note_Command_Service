71ef1f273570e333e65c77efeea85ba25ccdd5e4
codercengiz <codercengiz@gmail.com>
2021-03-22 09:57:17 +0100

    Add application settings mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![type_length_limit = "2297183"]
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate clap;
+
+mod settings;
+
+use crate::settings::Settings;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+    let settings = Settings::init();
+    
+    
 }
---
+++ src/settings/mod.rs
@@ -0,0 +1,89 @@
+use clap::{App, ArgMatches};
+
+use std::fmt::Debug;
+use std::net::SocketAddr;
+
+pub(crate) struct Settings {
+    web_service_settings: WebServiceSettings,
+    kafka_settings: KafkaSettings,
+    eventstore_settings: EventstoreSettings,
+}
+
+impl Settings {
+    pub(crate) fn init() -> Settings {
+        let yaml = load_yaml!("app_settings.yaml");
+        let matches = App::from(yaml).get_matches();
+        Settings {
+            web_service_settings: Self::get_web_service_settings(&matches),
+            kafka_settings: Self::get_kafka_settings(&matches),
+            eventstore_settings: Self::get_eventstore_settings(&matches),
+        }
+    }
+
+    fn get_web_service_settings(matches: &ArgMatches) -> WebServiceSettings {
+        let domain = matches
+            .value_of("domain")
+            .expect("Invalid Domain")
+            .to_string();
+        let port = matches.value_of("port").expect("Invalid Port").to_string();
+        let addr_string = format!("{}:{}", domain, port);
+        let socket_addr = addr_string
+            .parse()
+            .expect("Invalid Socket Address");
+
+        WebServiceSettings { socket_addr }
+    }
+
+    fn get_kafka_settings(matches: &ArgMatches) -> KafkaSettings {
+        KafkaSettings {
+            brokers: matches
+                
+                .value_of("kafka-brokers-addr")
+                .expect("Invalid Brokers")
+                .to_string(),
+
+            topics: matches
+                .values_of("kafka-topics")
+                .expect("Invalid Topics")
+                .map(|x| x.to_string())
+                .collect(),
+        }
+    }
+
+    fn get_eventstore_settings(matches: &ArgMatches) -> EventstoreSettings {
+        EventstoreSettings {
+            socket_addr: matches
+                .value_of("eventstore-socket-addr")
+                .expect("Invalid eventstore addr")
+                .parse()
+                .expect("Unable to parse socket address"),
+
+            username: matches
+                .value_of("eventstore-username")
+                .expect("Invalid Eventstore Username")
+                .to_string(),
+            password: matches
+                .value_of("eventstore-password")
+                .expect("Invalid Eventstore Password")
+                .to_string(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct WebServiceSettings {
+    pub(crate) socket_addr: SocketAddr,
+}
+
+#[derive(Debug)]
+pub(crate) struct KafkaSettings {
+    pub(crate) brokers: String,
+    pub(crate) topics: Vec<String>,
+}
+
+#[derive(Debug)]
+pub(crate) struct EventstoreSettings {
+    pub(crate) socket_addr: SocketAddr,
+    pub(crate) username: String,
+    pub(crate) password: String,
+}
